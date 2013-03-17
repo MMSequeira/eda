44413de559f8e0c10d449bfb9018b772264d0b10
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2013-03-17 18:28:57 +0000

    Add docco documentation.

--- eda/wow.c
+++ eda/wow.c
@@ -1,3 +1,5 @@
+// A demonstration of some C array and pointer rules
+// =================================================
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,13 +9,13 @@ int main_wow(void)
 	int m[] = {1, 2, 3};
 
 	for (int i = 0; i != 3; i++)
-//		printf("%d\n", m[i]);
-//		printf("%d\n", *(m + i));
-//		printf("%d\n", *(i + m));
+		// printf("%d\n", m[i]);
+		// printf("%d\n", *(m + i));
+		// printf("%d\n", *(i + m));
 		printf("%d\n", i[m]);
 
 	for (int i = 0; i != 5; i++)
-//		putchar("Hello"[i]);
+		// putchar("Hello"[i]);
 		putchar(i["Hello"]);
 
 	return EXIT_SUCCESS;
