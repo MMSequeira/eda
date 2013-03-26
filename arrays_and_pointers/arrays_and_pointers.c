fab9754b5a314246c3284e488d6de1129fbbb766
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2013-03-26 13:57:55 +0000

    Structural improvements.

--- arrays_and_pointers/arrays_and_pointers.c
+++ arrays_and_pointers/arrays_and_pointers.c
@@ -1,5 +1,5 @@
 // `arrays_and_pointers.c`
-// ======================
+// =======================
 /**
  * \file arrays_and_pointers.c
  * \brief Illustrates the relation between arrays and pointers in C.
@@ -9,31 +9,34 @@
  * regarding arrays, pointers, and indexing to produce silly and obscure code. 
  */
 
-// Regras do C sobre _arrays_ e ponteiros
-// =====================================================================
-//
-// A linguagem C tem uma forma perfeitamente consistente, embora
-// idiossincrática, de lidar com _arrays_ e ponteiros. Algumas das regras usadas
-// pelo C são:
-//
-// 1. Sempre que o nome de um _array_ surge numa expressão (com algumas
-//    excepções, tal como quando operando do operador `sizeof`), é convertido
-//    num ponteiro para o seu primeiro item. Assim, se `m` for um _array_, então
-//    `m` e `&m[0]` são expressões equivalentes.
-// 2. A utilização do operador de indexação, e.g., na expressão `m[i]`, é
-//    equivalente à «desreferenciação» do ponteiro obtido da soma entre o
-//    objecto indexado e o índice usado. Assim, `m[i]` tem exactamente o mesmo
-//    significado que `*(m + 1)`, ou seja, quando `m` for um _array_, `m[i]` é
-//    exactamente o mesmo que `*(&m[0] + 1)`.
-//
-// O programa abaixo evidencia estas regras pelo absurdo. Por favor, não tire
-// partido das regras enunciadas para escrever código incompreensível!
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
+	// Regras do C sobre _arrays_ e ponteiros
+	// --------------------------------------
+	//
+	// A linguagem C tem uma forma perfeitamente consistente, embora
+	// idiossincrática, de lidar com _arrays_ e ponteiros. Algumas das
+	// regras usadas pelo C são:
+	//
+	// 1. Sempre que o nome de um _array_ surge numa expressão (com algumas
+	// excepções, tal como quando operando do operador `sizeof`), é
+	// convertido    num ponteiro para o seu primeiro item. Assim, se `m`
+	// for um _array_, então    `m` e `&m[0]` são expressões equivalentes.
+	//
+	// 2. A utilização do operador de indexação, e.g., na expressão `m[i]`,
+	// é    equivalente à «desreferenciação» do ponteiro obtido da soma
+	// entre o    objecto indexado e o índice usado. Assim, `m[i]` tem
+	// exactamente o mesmo    significado que `*(m + 1)`, ou seja, quando
+	// `m` for um _array_, `m[i]` é exactamente o mesmo que `*(&m[0] +
+	// 1)`.
+	//
+	// O programa abaixo evidencia estas regras pelo absurdo. Por favor, não
+	// tire partido das regras enunciadas para escrever código
+	// incompreensível!
+	  
 	// O nome `m` é o nome de uma matriz com três números inteiros: 1, 2 e
 	// 3.
 	int m[] = {1, 2, 3};
