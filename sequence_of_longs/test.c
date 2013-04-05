bfab251c95e5da1cfa764df92f00887acd9b7c38
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2013-04-05 16:41:46 +0100

    Add project with first steps of implementation of ADT for sequence of longs.

---
+++ sequence_of_longs/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sequence_of_longs {
+	long* items;
+	int length;	;
+	;
+};
+
+void SEQL_println(struct sequence_of_longs s)
+{
+	printf("%d %p\n", s.length, s.items);
+//	printf("%d %p %ld\n", s.length, s.items, *s.items);
+}
+
+int main(void)
+{
+//	struct sequence_of_longs sequence;
+	struct sequence_of_longs sequence = {};
+//	struct sequence_of_longs sequence = {
+//		items: NULL,
+//		length: 0
+//	};
+
+	SEQL_println(sequence);
+
+	return EXIT_SUCCESS;
+}
