04a20c9e8995bc1d36207664d5efc36a000d4891
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2013-04-05 16:40:31 +0100

    Add project with example use of structs for rational numbers.

---
+++ rationals_with_structs/rationals_with_structs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct rational {
+	int numerator;
+	int denominator;
+};
+
+struct rational RAT_symmetric_of(struct rational r)
+{
+	struct rational result = {
+		numerator: -r.numerator,
+		denominator: r.denominator
+	};
+	return result;
+}
+
+void RAT_println(struct rational r)
+{
+	printf("%d/%d\n", r.numerator, r.denominator);
+}
+
+int main(void)
+{
+	struct rational r1 = { numerator: 1, denominator: 2};
+	// rational r2 = -r1;
+	struct rational r2 = RAT_symmetric_of(r1);
+
+	RAT_println(r1);
+	RAT_println(r2);
+
+	return EXIT_SUCCESS;
+}
