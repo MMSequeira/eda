d6d940f20905c5194eaf085904d8edbf83d80099
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2013-04-05 07:20:52 +0100

    Improve fibonacci implementation and documentation.

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -53,16 +53,13 @@
 //   é igual ao número de casais férteis no mês anterior e que, por isso, é
 //   igual ao número total de casais existentes dois meses antes.
 //
-// Sendo ![F(n)](http://latex.codecogs.com/gif.latex?%5Cinline%20F_n) o número
-// de casais de coelhos no mês
-// ![n](http://latex.codecogs.com/gif.latex?%5Cinline%20n), então, desde que
-// ![n>2](http://latex.codecogs.com/gif.latex?%5Cinline%20n%3E2), ![F(n)=F(n-2)+
-// F(n-1)](http://latex.codecogs.com/gif.latex?%5Cinline%20F_n%3DF_%7Bn-2%7D&plus;F_%7Bn-1%7D). Quando
-// ![n=1](http://latex.codecogs.com/gif.latex?%5Cinline%20n%3D1) ou
-// ![n=2](http://latex.codecogs.com/gif.latex?%5Cinline%20n%3D2),
-// ![F(n)=1](http://latex.codecogs.com/gif.latex?%5Cinline%20F_n%3D1). Ou seja,
-// esta sucessão pode ser descrita de forma _recursiva_, ou seja, definindo os
-// termos da sucessão em função de outros termos da mesma sucessão.
+// Sendo ![F(n)](http://bit.ly/ZCdcqY) o número de casais de coelhos no mês
+// ![n](http://bit.ly/Z4ELdk), então, desde que ![n>2](http://bit.ly/10vWeOB),
+// ![F(n)=F(n-2)+F(n-1)](http://bit.ly/10zLPR3). Quando
+// ![n=1](http://bit.ly/14HsgeM) ou ![n=2](http://bit.ly/YVICg2),
+// ![F(n)=1](http://bit.ly/13V3GYG). Ou seja, esta sucessão pode ser descrita de
+// forma _recursiva_, ou seja, definindo os termos da sucessão em função de
+// outros termos da mesma sucessão.
 //
 // O que pretendemos aqui é implementar uma função que receba o mês como
 // argumento e devolva o número de casais existentes nesse mês. Como não
@@ -77,17 +74,17 @@
 // implementação.
 //
 // Antes, porém, de passar às implementações, é preciso dizer que o presente
-// oferecido ao Sr. Fibonacci está realmente envenenado. Com efeito, a sequência
+// oferecido ao Sr. Fibonacci está realmente envenenado. Com efeito, a sucessão
 // de Fibonacci cresce de forma extremamente rápida. Assim, no mês 120, ou seja,
 // dez anos depois da oferta do casal original, o número de casais a alimentar é
 // exactamente 5&thinsp;358&thinsp;359&thinsp;254&thinsp;990&thinsp;966&thinsp;640&thinsp;871&thinsp;840.
 // Este valor é demasiado grande para os `int` nas nossas máquinas habituais,
 // que têm 32 _bits_, ou mesmo para os _long_, com 64 _bits_ nessas mesmas
 // máquinas. Por outro lado, a precisão limitada dos tipos de virgula flutuante
-// pode pôr problemas complicados ao cálculo dos termos da sequência (já
+// pode pôr problemas complicados ao cálculo dos termos da sucessão (já
 // tínhamos dito que dos tipos de vírgula flutuante se foge como diabo da
 // cruz?). O código abaixo recorre ao tipo `long`, numa tentativa de evitar que
-// se atinjam muito cedo os limites dos inteiros. É uma ná solução, no entanto.
+// se atinjam muito cedo os limites dos inteiros. É uma má solução, no entanto.
 // Uma boa solução passaria pela utilização de um tipo capaz de representar
 // números inteiros de dimensão arbitrária (desde que haja memória para o
 // guardar, bem entendido). A implementação de um tal tipo é um excelente
@@ -96,6 +93,41 @@
 // Implementação
 // -------------
 //
+// Como se referiu, usamos `long` como tipo de devolução das funções de cálculo
+// dos termos da sucessão de Fibonacci. Será que isso adianta muito, em relação
+// à utilização de _int_? Fazendo algumas experiências, é fácil verificar que,
+// em máquinas em que os `int` têm 32 _bits_, ou seja, em máquinas em que o
+// maior valor representável num `int` é 2&thinsp;147&thinsp;483&thinsp;647, o
+// maior termo da sucessão de Fibonacci representável num `int` é o termo 46, ou
+// seja, ![F(46)](http://bit.ly/XgffV7), cujo valor é
+// 1&thinsp;836&thinsp;311&thinsp;903. Isto é, usando `int` só conseguimos obter
+// valores correctos para os primeiros 47 termos da sucessão. Usando `long`, é
+// fácil verificar que, em máquinas em que os `long` têm 64 _bits_, ou seja, em
+// máquinas em que o maior valor representável num `long` é
+// 9&thinsp;223&thinsp;372&thinsp;036&thinsp;854&thinsp;775&thinsp;807, o maior
+// termo da sucessão de Fibonacci representável num `long` é o termo 92, ou
+// seja, ![F(92)](http://bit.ly/12kthcr), cujo valor é
+// 7&thinsp;540&thinsp;113&thinsp;804&thinsp;746&thinsp;346&thinsp;429. Isto é,
+// usando `long` só conseguimos obter valores correctos para os primeiros 93
+// termos da sucessão. Melhor que os 47 se se usasse `int`, mas ainda assim um
+// valor bem pequeno.
+//
+// O maior termo calculável usando `long`, 92, deve ser usado nas pré-condições
+// das funções abaixo. Usar o valor literal nesse código é muito má ideia, pois
+// torna mais difícil a compreensão do código. Afinal, haverá que perceba
+// imediatamente o significado desse valor? Para resolver o problema definimos
+// uma constante para guardar esse valor. Uma vez que esta constante será usada
+// para dimensionar _arrays_ que pretendemos inicializar usando as usuais listas
+// de inicializadores do C, não podemos usar a forma mais óbvia de definir
+// constantes em C, i.e., não podemos usar
+//
+// ```C
+// const int maximum_term_fitting_a_long = 92;
+// ```
+//
+// tendo, pelo contrário de usar uma macro do pré-processador.
+#define MAXIMUM_TERM_FITTING_A_LONG 92
+
 // ### Inclusão de ficheiros de cabeçalho
 //
 // Incluímos os seguintes ficheiros de cabeçalho:
@@ -138,30 +170,86 @@
  * \f]
  */
 // #### Definição
+//
+// Esta implementação segue a estratégia mais comum das soluções recursivas:
+// decompor o problema em problemas mais simples, resolver esses problemas mais
+// simples usando exactamente a mesma estratégia, recursivamente, e compor as
+// soluções dos subproblemas de modo a obter a solução do problema como um todo.
+// Uma solução recursiva, como esta, precisa de possuir casos de paragem (ou
+// casos especiais), para os quais a solução é imediata, não sendo necessária
+// qualquer decomposição adicional. A definição recursiva da sucessão de
+// Fibonacci aparenta adequar-se bem a esta estratégia:
+//
+// ![F(n)={0, se n = 0, 1, se n = 1, e F(n-2) + F(n-1), se n > 1}](http://bit.ly/13TPIpP)
 long stupidly_recursive_fibonacci(int n)
 {
+	// Em primeiro lugar, verificamos as pré-condições e lidamos com as
+	// violações que forem detectadas.
 	assert(n >= 0);
+	assert(n <= MAXIMUM_TERM_FITTING_A_LONG);
 
+	// Depois, lidamos com os dois casos especiais da sucessão. Estes casos
+	// servem também para garantir que as invocações recursivas desta função
+	// acabarão sempre por terminar (em tempo finito).
 	if (n == 0)
 		return 0L;
 	if (n == 1)
 		return 1L;
 
+	// O caso geral é simplesmente a devolução da soma dos termos anterior
+	// da sucessão. Para os obter, invocamos a própria função com os dois
+	// valores inteiros anteriores a `n`. O problema desta implenentação é
+	// que se está a reduzir o problema de calcular
+	// ![F(n)](http://bit.ly/ZCdcqY) aos subproblemas de calcular
+	// ![F(n-2)](http://bit.ly/14HrTAK) e de calcular
+	// ![F(n-1)](http://bit.ly/13V2Vid), _mas estes subproblemas não são
+	// independentes!_
 	return stupidly_recursive_fibonacci(n - 2)
 		+ stupidly_recursive_fibonacci(n - 1);
 }
 
+// ### Implementação recursiva com _lookup_
+
+// #### Documentação
+// A documentação da função é feita no formato do
+// [Doxygen](http://doxygen.org/), como habitualmente.
+/** \brief Returns the `n`th term of the Fiboncci sequence.
+ *
+ * \param n The number of the term to return (first valid value is 0).
+ * \return The value of the `n`th term of the Fibonacci sequence. The time taken
+ * by the function grows !!!!!!!!!!!!!!!!!!!! with `n`.
+ * \pre `n` ≥ 0
+ * \post result = \f$F_{\mathtt{n}}\f$
+ *
+ * Returns the `n`th term of the Fibonacci sequence. It is assumed the sequence
+ * starts at `n` = 0, with value 0, followed by value 1, at `n` = 1. That is,
+ * the sequence is defined by
+ * \f[
+ * F_n = \left\{\begin{array}{ll}
+ *     0                 & \text{if } n=0, \\
+ *     1                 & \text{if } n=1\text{, and} \\
+ *     F_{n-2} + F_{n-1} & \text{if } n>1.
+ *   \end{array}\right.
+ * \f]
+ */
+// #### Definição
 long recursive_fibonacci(int n)
 {
-	static long F[93] = {0, 1};
+	// Em primeiro lugar, verificamos as pré-condições e lidamos com as
+	// violações que forem detectadas.
+	assert(n >= 0);
+	assert(n <= MAXIMUM_TERM_FITTING_A_LONG);
+
+	// !!!!!!!!!!!! Inicialização e static. Casos especiais.
+	static long F[MAXIMUM_TERM_FITTING_A_LONG + 1] = {0, 1};
 	static int number_of_calculated_terms = 2;
 
-	assert(n >= 0);
-	assert(n < 93);
-
+	// Se o termo já constar na memória de termos calculados, limitamo-nos
+	// a devolvê-lo.
 	if (n < number_of_calculated_terms)
 		return F[n];
 
+	// O caso geral passa por invocar recursivamente !!!!!!! ordem de cálculo é relevante?!!!!!!!
 	number_of_calculated_terms++;
 	return F[n] = recursive_fibonacci(n - 2) + recursive_fibonacci(n - 1);
 //	number_of_calculated_terms = n + 1;
@@ -195,6 +283,7 @@ long recursive_fibonacci(int n)
 long iterative_fibonacci(int n)
 {
 	assert(n >= 0);
+	assert(n <= MAXIMUM_TERM_FITTING_A_LONG);
 
 	if (n == 0)
 		return 0L;
