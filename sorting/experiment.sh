a2b5220da3980d5fd5be5918a9976ea139d72cfa
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2013-12-03 14:25:49 +0000

    Change order of experiments.

--- sorting/experiment.sh
+++ sorting/experiment.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
+bin/Release/sorting ../../sort-data/ shuffled shuffled_results.csv
 bin/Release/sorting ../../sort-data/ partially_sorted partially_sorted_results.csv
 bin/Release/sorting ../../sort-data/ sorted sorted_results.csv
-bin/Release/sorting ../../sort-data/ shuffled shuffled_results.csv
